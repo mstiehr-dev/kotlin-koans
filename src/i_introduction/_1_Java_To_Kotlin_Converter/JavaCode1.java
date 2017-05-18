6f15633e14596c2430b403becb9df6f972a39767
unknown <MStiehr@BORSBERG.itc2.local>
2017-05-18 16:13:08 +0200

    solve task #1

--- src/i_introduction/_1_Java_To_Kotlin_Converter/JavaCode1.java
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/JavaCode1.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import java.util.Iterator;
 
 public class JavaCode1 extends JavaCode {
-    public String task1(Collection<Integer> collection) {
+    public String todoTask1(Collection<Integer> collection) {
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         Iterator<Integer> iterator = collection.iterator();
